12bd1580a85518e16a2fd32fbbdee035c4a42581
fjij <30779570+fjij@users.noreply.github.com>
2021-01-22 21:09:52 -0500

    Write test GET /server

--- web/test/server.js
+++ web/test/server.js
@@ -1,3 +1,4 @@
+const db = require('../src/db');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../src');
@@ -5,14 +6,45 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+let serverSeed = 0;
+async function mockServer() {
+  const server = {
+    name: `server${serverSeed}`,
+    created: new Date(Date.now() + 1000*serverSeed),
+    port: 1234 + serverSeed,
+    path: `/srv/minecraft/server${serverSeed}`
+  };
+  await db.query(
+    'INSERT INTO server (name, created, port, path) VALUES ($1, $2, $3, $4)',
+    [server.name, server.created, server.port, server.path]
+  );
+  serverSeed ++;
+  return server;
+}
+
+async function clearServerTables() {
+  await Promise.all([
+    db.query('DELETE FROM server'),
+    db.query('DELETE FROM server_env'),
+  ]);
+}
+
 describe('Server', () => {
 
-  describe('/GET server/helloworld', () => {
+  afterEach(clearServerTables);
 
-    it('should give us a nice message', async () => {
-      const res = await chai.request(app).get('/server/helloworld');
+  describe('/GET server', () => {
+
+    it('should list servers', async () => {
+      const servers = [
+        await mockServer(),
+        await mockServer()
+      ];
+      const res = await chai.request(app).get('/server');
       res.should.have.status(200);
-      res.text.should.be.eql('Hello :)');
+      res.body.should.be.eql({
+        servers
+      });
     });
 
   });
